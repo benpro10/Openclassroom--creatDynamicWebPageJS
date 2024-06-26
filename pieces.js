7734e2ad882102d7fec425b85e729dc6830508ef
Exaucé Benjamin Kyalondawa <exaucebenjamink@gmail.com>
2024-06-26 18:25:35 +0100

    manipulate the dom on my project

--- pieces.js
+++ pieces.js
@@ -1,19 +1,29 @@
 // Récupération des pièces depuis le fichier JSON
-const reponse = await fetch('pieces-autos.json');
+const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
-// Création des balises 
+// Création des balises
 const article = pieces[0];
 const imageElement = document.createElement("img");
 imageElement.src = article.image;
 const nomElement = document.createElement("h2");
 nomElement.innerText = article.nom;
 const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+prixElement.innerText = `Prix: ${article.prix} € (${
+  article.prix < 35 ? "€" : "€€€"
+})`;
 const categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+const description = document.createElement("p");
+description.innerText = article.description ?? "Pas de description";
+const disponibility = document.createElement("p");
+disponibility.innerText = `${article.nom} est en ${
+  article.disponibilite === true ? "Stock" : "Rupture de stock"
+}`;
 //Rattachement de nos balises au DOM
 const sectionFiches = document.querySelector(".fiches");
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(description);
+sectionFiches.appendChild(disponibility);
